a0ef4ebaa008f28ceef8043f491947e8770c8550
гера шлычков <germean322@gmail.com>
2021-09-15 22:12:07 +1000

    Задание 2

---
+++ Task2/CosRowObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task2
+{
+    public class CosRowObject
+    {
+        private const int count = 6;
+
+        private double cosTrueValue;
+
+        private readonly double _argument;
+
+        private int[] _iteration = new int[count];
+
+        private double[] funValue = new double[count];
+
+        private double[] _accuracy = { 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001 };
+
+        public CosRowObject(double argument)
+        {
+            _argument = argument;
+
+            cosTrueValue = Math.Cos(_argument);
+        }
+
+        public double Argument => _argument;
+
+        public int[] Iteration => _iteration;
+
+        public double[] FunValue => funValue;
+
+        public double[] Accuracy => _accuracy;
+
+        public void ComputeAll()
+        {
+
+            for (int i = 0; i < _accuracy.Length; i++)
+            {
+                Compute(_accuracy[i], i);
+            }
+           
+        }
+
+        private void Compute(double accuracy, int itr)
+        {
+            double sum = 100;
+
+            int itter = 0;
+
+            while (Math.Abs(sum - cosTrueValue) > accuracy)
+            {
+                if (itter == 12)
+                    break;
+
+                if (itter == 0)
+                    sum = 0;
+
+                sum += Math.Pow(-1, itter) * (Math.Pow(_argument, 2*itter) / F(2 * itter) );
+
+                itter++;
+            }
+
+            _iteration[itr] = itter;
+
+            funValue[itr] = Math.Round(sum, 5);
+        }
+
+
+        private long F(int f)
+        {
+            long m = 1;
+
+            for (int i = 1; i <= f; i++)
+            {
+                m *= i;
+            }
+
+            return m;
+        }
+
+    }
+}
---
+++ Task2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+
+        private const string formText = "Итерационное вычесление сумм";
+
+        private const string button1Text = "Вычислить";
+        private const string button2Text = "Прочитать из файла";
+        private const string button3Text = "Выход";
+
+        private const string label1Text = "Введите значение аргумента \nв градусах";
+        private const string label2Text = "Начальное";
+        private const string label3Text = "Конечное";
+
+        private const int argumentsCount = 10;
+
+        private CosRowObject[] objs;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = formText;
+
+            button1.Text = button1Text;
+            button2.Text = button2Text;
+            button3.Text = button3Text;
+
+            label1.Text = label1Text;
+            label2.Text = label2Text;
+            label3.Text = label3Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var firstResult = double.TryParse(textBox1.Text, out var firstBord);
+
+            var endResult = double.TryParse(textBox2.Text, out var endBord);
+
+            if( ! (firstResult && endResult))
+            {
+                MessageBox.Show("Введены некорректные данные(", "Ошибка");
+
+                return;
+            }
+
+            ComputeCosRowObject(firstBord, endBord);
+
+            WriteInfo();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) => 
+            Close();
+
+        private void ComputeCosRowObject(double firstBorder, double endBorder)
+        {
+
+            objs = new CosRowObject[argumentsCount];
+
+            var args = GetArguments(firstBorder, endBorder);
+
+            for (int i = 0; i < argumentsCount; i++)
+            {
+                objs[i] = new CosRowObject(args[i]);
+
+                objs[i].ComputeAll();
+            }
+
+        }
+
+        private double[] GetArguments(double firstBorder, double endBorder)
+        {
+            var step = (endBorder - firstBorder) / argumentsCount;
+
+            double[] args = new double[argumentsCount];
+
+            for (int i = 0; i < argumentsCount; i++)
+            {
+                args[i] = firstBorder + step * i;
+
+                args[i] = Math.Round(args[i], 2);
+            }
+
+            return args;
+        } 
+
+        private void WriteInfo()
+        {
+            listBox1.Items.Add("Аргумент   Точность  Функция    Итерация");
+
+            foreach (var obj in objs)
+            {
+                
+                for (int i = 0; i < obj.FunValue.Length; i++)
+                {
+                    listBox1.Items.Add($"{obj.Argument}\t  {obj.Accuracy[i]}\t  " +
+                        $"{obj.FunValue[i]}\t\t  {obj.Iteration[i]}\t");
+                }
+
+                listBox1.Items.Add("");
+            }
+        }
+
+    }
+}
