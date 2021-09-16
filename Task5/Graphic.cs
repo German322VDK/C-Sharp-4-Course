ee2f22009c548ecce92795ec5c0ce24fbf71c80e
гера шлычков <germean322@gmail.com>
2021-09-16 22:42:39 +1000

    Задание 5

---
+++ Task5/Graphic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Task5
+{
+    public class Graphic
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public List<int> Iterations { get; set; } = new List<int>();
+
+        public List<double> Accuracies { get; set; } = new List<double>();
+
+    }
+}
