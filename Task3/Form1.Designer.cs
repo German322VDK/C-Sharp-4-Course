17410bb4522475cbaa51ae26ba326399ff840127
гера шлычков <germean322@gmail.com>
2021-09-16 13:04:37 +1000

    Задание 3

--- Task3/Form1.Designer.cs
+++ Task3/Form1.Designer.cs
@@ -31,11 +31,20 @@ namespace Task3
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(298, 329);
+            this.button1.Location = new System.Drawing.Point(531, 213);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +54,7 @@ namespace Task3
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(408, 404);
+            this.button2.Location = new System.Drawing.Point(531, 269);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -53,16 +62,101 @@ namespace Task3
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(323, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(323, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 23);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(323, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(123, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(531, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(531, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "5";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(23, 213);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(423, 79);
+            this.listBox1.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 450);
+            this.ClientSize = new System.Drawing.Size(655, 339);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +164,15 @@ namespace Task3
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Task3/Form1.cs
+++ Task3/Form1.cs
@@ -12,17 +12,127 @@ namespace Task3
 {
     public partial class Form1 : Form
     {
+        private const string label1Text = "Введите основание первой системы счисления";
+        private const string label2Text = "Введите значение разрядов числа через пробел";
+        private const string label3Text = "Введите основание второй системы счисления";
+        private const string label4Text = "p =";
+        private const string label5Text = "q =";
+
         public Form1()
         {
             InitializeComponent();
+
+            label1.Text = label1Text;
+            label2.Text = label2Text;
+            label3.Text = label3Text;
+            label4.Text = label4Text;
+            label5.Text = label4Text;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var pResult = int.TryParse(textBox1.Text, out var p);
+
+            var qResult = int.TryParse(textBox3.Text, out var q);
+
+            var nums = textBox2.Text.Split(" ");
+
+            bool[] numResult = new bool[nums.Length];
+
+            int[] numbers = new int[nums.Length];
+
+            bool result = pResult & qResult & p >= 2 & p <= 10000 & q >= 2 & q <= 10000;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numResult[i] = int.TryParse(nums[i], out numbers[i]);
+
+                if (numResult[i])
+                {
+                    if(numbers[i] >= p || (numbers[i] < 0) )
+                        numResult[i] = false;
+                }
+                   
+                result &= numResult[i];
+            }
+
+            if (!result)
+            {
+                MessageBox.Show("Введены некорректные данные", "Ошибка");
+
+                return;
+            }
+
+            string pSplitter = GetSplitter(p);
+            string qSplitter = GetSplitter(q);
+
+            var oldNum = string.Join<int>(pSplitter, numbers);
+
+            int num10 = FromNumeralSystemTo10(numbers, p);
+
+            var newNum = From10ToNumeralSystem(num10, q, qSplitter);
+
+            label4.Text = $"{label4Text} {p}";
+            label5.Text = $"{label4Text} {q}";
+
+            listBox1.Items.Add($"{p}ая система счисления, число: {oldNum}");
+            listBox1.Items.Add($"{10}ая система счисления, число: {num10}");
+            listBox1.Items.Add($"{q}ая система счисления, число: {newNum}");
+            listBox1.Items.Add("");
 
         }
 
         private void button2_Click(object sender, EventArgs e) =>
             Close();
+
+        private int FromNumeralSystemTo10(int[] numbers, int sys)
+        {
+            int num10 = 0;
+            int itr = 0;
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                num10 += numbers[i] * (int)Math.Pow(sys, itr);
+
+                itr++;
+            }
+
+            return num10;
+        }
+
+        private string From10ToNumeralSystem(int tmp, int sys, string splitter)
+        {
+            int temp1 = 0;
+            var s = new List<int>();
+            while (tmp > 0)
+            {
+                temp1 = tmp % sys;
+                tmp = tmp / sys;
+                s.Add(temp1);
+            }
+            return Reverse(s, splitter);
+        }
+
+        private string Reverse(List<int> norm, string splitter)
+        {
+            int[] s = new int[norm.Count];
+
+            for (int i = norm.Count - 1; i >= 0; i--)
+            {
+                s[norm.Count - 1 - i] = norm[i];
+            }
+
+            return string.Join<int>(splitter, s);
+        }
+
+        private string GetSplitter(int n)
+        {
+            if (n > 9)
+                return ".";
+            else
+                return "";
+
+        }
+
     }
 }
